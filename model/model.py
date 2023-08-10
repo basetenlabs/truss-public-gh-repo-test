0338456cb1a1bb3386bfb03ecfb19ff2953dde62
Sid Shanker <sid.shanker@baseten.co>
2023-08-10 17:04:39 -0400

    Streaming example.

--- model/model.py
+++ model/model.py
@@ -4,26 +4,15 @@ import time
 
 class Model:
     def __init__(self, **kwargs) -> None:
-        self._data_dir = kwargs["data_dir"]
-        if (self._data_dir / 'test_blob').exists():
-            print('External blob found')
-        else:
-            print('External blob not found')
-        self._config = kwargs["config"]
-        self._model = None
-    
+        pass
+
     def load(self):
-        print("LOADING MODEL 3")
-        print("foobyfooo")
-        time.sleep(20)
+        pass
 
 
     def predict(self, request: Dict) -> Dict[str, List]:
-        response = {}
-
-        inputs = request["inputs"]  # noqa
-        if inputs == [1]:
-            raise Exception("you did a bad")
-        # Invoke model and calculate predictions here.
-        response["predictions"] = inputs
-        return response
+        def inner():
+            for i in range(5):
+                time.sleep(1)
+                print(str(i))
+        return inner()
